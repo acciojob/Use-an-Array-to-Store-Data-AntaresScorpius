895447fc50f02d422cb9d2b7e6ace405d4c1d685
AntaresScorpius <rohitc171@gamil.com>
2022-07-08 22:20:13 +0530

    Modified Array

--- solution.js
+++ solution.js
@@ -2,6 +2,8 @@
 let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+yourArray = [1,"two",true,false,null,undefined];
+
 // code end
 
 module.exports = yourArray
